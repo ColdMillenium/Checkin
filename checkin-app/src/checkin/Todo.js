093a4314074b5b27443a153e80b9a5ceb15b11ab
ColdMillenium <andrewgerribrowning@gmail.com>
2020-11-11 01:43:20 -0500

    playing around with drag on cards

--- checkin-app/src/checkin/Todo.js
+++ checkin-app/src/checkin/Todo.js
@@ -5,6 +5,7 @@ import CardActions from '@material-ui/core/CardActions';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import {ReactComponent as DeleteIcon} from '../icons/delete.svg'
+import {motion} from 'framer-motion';
 
 
 const useStyles = makeStyles({
@@ -64,11 +65,17 @@ export default function Todo(props) {
     }
 
     return(
-        <Card onClick={selectTodo} className={style()}>
-            <CardContent className={classes.text}>
-                {Content()}
-            </CardContent>
-        </Card>   
+        <motion.div
+            drag
+            dragConstraints={{left:0, top:0, right:0, bottom:0}}
+        >
+            <Card onClick={selectTodo} className={style()}>
+                <CardContent className={classes.text}>
+                    {Content()}
+                </CardContent>
+            </Card>  
+        </motion.div>
+         
     )
     
 }
