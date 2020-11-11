866d586ae8b0a80e1bed6173cd905c3d541995db
ColdMillenium <andrewgerribrowning@gmail.com>
2020-11-11 01:39:41 -0500

    playing around with Drag on homepage

--- checkin-app/src/Main.js
+++ checkin-app/src/Main.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { Button } from '@material-ui/core';
 import {useStoreState, useStoreActions} from 'easy-peasy'
 import Account from './account/Account'
 import TodoList from './checkin/TodoList'
 import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
-import {Demo} from './common/ClickOutide'
+import {motion} from 'framer-motion'
+
 
 export default function Main() {
     const authenticated = useStoreState((state) => state.authenticated);
-    
+    const constraintsRef = useRef(null);
     return (<div>
      
      <Router>
@@ -31,6 +32,16 @@ export default function Main() {
       </Switch>
     </Router>
     <hr />
+    <motion.div
+      ref={constraintsRef}
+      style={{ background: "green", width: 200, height: 200 }}
+    >
+      <motion.div
+        drag
+        dragConstraints={constraintsRef}
+        style={{ background: "red", width: 100, height: 100 }}
+      />
+    </motion.div>
     </div>);
 }
 
