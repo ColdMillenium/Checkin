f5a7de423aae481f72762153a8258cc6b383ae98
ColdMillenium <andrewgerribrowning@gmail.com>
2020-11-11 22:11:30 -0500

    using grid to align Account.js and center items!

--- checkin-app/src/account/Account.js
+++ checkin-app/src/account/Account.js
@@ -4,31 +4,40 @@ import SignIn from './SignIn'
 import {BrowserRouter as Router, Switch, Route, Redirect, useHistory} from 'react-router-dom';
 import { motion } from "framer-motion"
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid'
 
 const arriveLeft = {
     hidden: { x: "-100vw" },
-    visible: { x: "50vw", transition: { duration: 0.5 } }
+    visible: { x: "0vw", transition: { duration: 0.5 } }
   };
   const arriveRight = {
     hidden: { x: "100vw" },
-    visible: { x: "50vw", transition: { duration: 0.5 } }
+    visible: { x: "0vw", transition: { duration: 0.5 } }
   };
 export default function Account() {
     const history = useHistory();
     return (
             <div>
-                <Typography variant="h2">
-                        Welcome to Checkin!
-                </Typography>
-                <Route exact path="/Account">
-                    <Redirect from="/" to="Account/SignIn"></Redirect>
-                </Route>
-                <Route path="/Account/SignIn">
-                        {Appear(SignIn, arriveLeft)} 
-                </Route>
-                <Route path="/Account/SignUp">
-                        {Appear(SignUp, arriveRight)}
-                </Route>
+                <Grid container direction="column" alignItems="center">
+                    <Grid item>
+                        <Typography variant="h2">
+                            Welcome to Checkin!
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Route exact path="/Account">
+                            <Redirect from="/" to="Account/SignIn"></Redirect>
+                        </Route>
+                        <Route path="/Account/SignIn">
+                            {Appear(SignIn, arriveLeft)} 
+                        </Route>
+                        <Route path="/Account/SignUp">
+                                {Appear(SignUp, arriveRight)}
+                        </Route>
+                    </Grid>
+                </Grid>
+                
+                
             </div>
            
      
