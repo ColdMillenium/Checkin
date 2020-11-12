7af8654009c870142af5eabb7c11ce5d0fbd8297
ColdMillenium <andrewgerribrowning@gmail.com>
2020-11-11 20:48:16 -0500

    got checkbox, name and delete icon on same page. Only text opens edit

--- checkin-app/src/checkin/Todo.js
+++ checkin-app/src/checkin/Todo.js
@@ -6,17 +6,18 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import {ReactComponent as DeleteIcon} from '../icons/delete.svg'
 import {motion} from 'framer-motion';
+import Grid from '@material-ui/core/Grid'
 
 
 const useStyles = makeStyles({
     root: {
-      maxWidth: 275,
+      maxWidth: 500,
       minWidth: 250,
       marginTop: 10,
       marginBottom:10,
     },
     rootSelected: {
-        maxWidth: 275,
+        maxWidth: 500,
         minWidth: 250,
         marginTop: 10,
         marginBottom:10,
@@ -50,20 +51,6 @@ export default function Todo(props) {
             return classes.root;
         }
     }
-    function Content(){
-        if(selected){
-            return (
-                <div>
-                    <p>{data.name}</p>
-                    <DeleteIcon/>
-                  
-                   
-                </div>
-                )
-        }else{
-            return <div>{data.name}</div>
-        }
-    }
 
     return(
         // <motion.div
@@ -72,13 +59,21 @@ export default function Todo(props) {
         // >
             <Card  className={style()}>
                 <CardContent className={classes.text}>
-                    {Content()}
+                <Grid container spacing= {2}>
+                    <Grid item>
+                        <CheckBox 
+                            checked={data.completed}
+                            onChange={()=>{completeTodo(data)}}
+                        />
+                    </Grid>
+                    <Grid onClick={selectTodo} item>
+                        <p>{data.name}</p>
+                    </Grid>
+                    <Grid item>
+                        <DeleteIcon/>
+                    </Grid>
+                </Grid>   
                 </CardContent>
-                <Button onClick={selectTodo}>Edit</Button>
-                <CheckBox 
-                checked={data.completed}
-                onChange={()=>{completeTodo(data)}}
-                />
             </Card>  
         // </motion.div>
          
