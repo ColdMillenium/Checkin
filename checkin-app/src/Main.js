033ee83efae74d2b6afd34dce7e46b2c89f810d4
ColdMillenium <andrewgerribrowning@gmail.com>
2020-11-11 21:51:33 -0500

    added a threme provider and set main text to Roboto

--- checkin-app/src/Main.js
+++ checkin-app/src/Main.js
@@ -1,47 +1,57 @@
 import React, {useRef} from 'react'
-import { Button } from '@material-ui/core';
 import {useStoreState, useStoreActions} from 'easy-peasy'
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import 'fontsource-roboto'
+
 import Account from './account/Account'
 import TodoList from './checkin/TodoList'
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
-import {motion} from 'framer-motion'
 
 
+
+
+//------------V----- App Styling ---------------V------//
+
+
+
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main:"#6a89cc"
+        },
+        secondary:{
+            main:"#b8e994"
+        }
+    }
+})
+//------------V----- High Level App Routing ---------------V------//
 export default function Main() {
     const authenticated = useStoreState((state) => state.authenticated);
     const constraintsRef = useRef(null);
-    return (<div>
-     
-     <Router>
-      <Switch>
-        <Route exact path="/">
-          <Redirect from="/" to="/Account"></Redirect>
-        </Route>
-        <Route path="/Account"><Account></Account></Route>
-        <Route path="/app">{
-            ()=>{
-                if(authenticated){
-                    return <TodoList></TodoList>
-                }else{
-                    return(
-                        <Redirect from="/app" to="/Account"></Redirect>
-                    )
+    return (
+    <ThemeProvider theme={theme}> 
+        <Router>
+        <Switch>
+            <Route exact path="/">
+            <Redirect from="/" to="/Account"></Redirect>
+            </Route>
+            <Route path="/Account"><Account></Account></Route>
+            <Route path="/app">{
+                ()=>{
+                    if(authenticated){
+                        return <TodoList></TodoList>
+                    }else{
+                        return(
+                            <Redirect from="/app" to="/Account"></Redirect>
+                        )
+                    }
                 }
-            }
-        }</Route>
-      </Switch>
-    </Router>
-    <hr />
-    <motion.div
-      ref={constraintsRef}
-      style={{ background: "green", width: 200, height: 200 }}
-    >
-      <motion.div
-        drag
-        dragConstraints={constraintsRef}
-        style={{ background: "red", width: 100, height: 100 }}
-      />
-    </motion.div>
-    </div>);
+            }</Route>
+        </Switch>
+        </Router>
+        <hr />
+    </ThemeProvider>);
 }
 
